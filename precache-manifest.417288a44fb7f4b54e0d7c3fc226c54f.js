635fd7eab41734530a42c4c78405ff79acdf7790
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-11 13:51:27 +0000

    Deploy zeroDevs/coding_challenge-13 to github.com/zeroDevs/coding_challenge-13.git:gh-pages

---
+++ precache-manifest.417288a44fb7f4b54e0d7c3fc226c54f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "572bf5731c52ba85adb3",
+    "url": "/coding_challenge-13/static/css/main.197c6cc0.chunk.css"
+  },
+  {
+    "revision": "572bf5731c52ba85adb3",
+    "url": "/coding_challenge-13/static/js/main.572bf573.chunk.js"
+  },
+  {
+    "revision": "bdaf85e98389efa9e631",
+    "url": "/coding_challenge-13/static/js/1.bdaf85e9.chunk.js"
+  },
+  {
+    "revision": "78d08d1ad98d4dab53eb",
+    "url": "/coding_challenge-13/static/js/runtime~main.78d08d1a.js"
+  },
+  {
+    "revision": "3f23df8c64386f23ada57e5c0c5d4b5f",
+    "url": "/coding_challenge-13/static/media/bg-whitewave-desktop.3f23df8c.svg"
+  },
+  {
+    "revision": "4c27a80f3956a02fe7d1df6cd5419948",
+    "url": "/coding_challenge-13/static/media/heart.4c27a80f.svg"
+  },
+  {
+    "revision": "3e3206c5f1e9618387aa0dc88188be48",
+    "url": "/coding_challenge-13/static/media/bg-wave-desktop.3e3206c5.svg"
+  },
+  {
+    "revision": "4cbbd78ce179d2e2e4de435f37ade66a",
+    "url": "/coding_challenge-13/static/media/parallax.4cbbd78c.svg"
+  },
+  {
+    "revision": "b740f4640d2e2c904757d0ffaa378b84",
+    "url": "/coding_challenge-13/static/media/logo.b740f464.psd"
+  },
+  {
+    "revision": "ebb669c47397a5a058fcb49bd4c09f0b",
+    "url": "/coding_challenge-13/static/media/logo1alt.ebb669c4.psd"
+  },
+  {
+    "revision": "28887e04832b876bdf736df0d29bbbab",
+    "url": "/coding_challenge-13/index.html"
+  }
+];
